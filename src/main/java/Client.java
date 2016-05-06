237d10ae8f68788b6b9bf0fb82261ef9e03b1279
jf <j.k.fokes@gmail.com>
2016-05-05 20:53:14 -0700

    add client class with full testing

---
+++ src/main/java/Client.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
+public class Client {
+	private int id;
+	private String name;
+	private String phone;
+	private String requestedDate;
+	private String clientRequests;
+	private int stylistId;
+
+	public Client(String name, String phone, String requestedDate, String clientRequests, int stylistId) {
+		this.name = name;
+		this.phone = phone;
+		this.requestedDate = requestedDate;
+		this.clientRequests = clientRequests;
+		this.stylistId = stylistId;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getRequestedDate() {
+		return requestedDate;
+	}
+
+	public String getClientRequests() {
+		return clientRequests;
+	}
+
+	public static List<Client> all() {
+    	try(Connection con = DB.sql2o.open()){
+      		String sql = "SELECT * FROM clients";
+      		return con.createQuery(sql).executeAndFetch(Client.class);
+    	}
+  	}
+
+
+	public void saveClientToStylist(int inputId) {
+	    try(Connection con = DB.sql2o.open()){
+		    String sql = "INSERT INTO clients(name, phone, requestedDate, clientRequests, stylistId) VALUES (:name, :phone, :requestedDate, :clientRequests, :stylistId)";
+		    this.stylistId = inputId;
+		    this.id = (int) con.createQuery(sql, true)
+		    .addParameter("name", this.name)
+		    .addParameter("phone", this.phone)
+		    .addParameter("requestedDate", this.requestedDate)
+		    .addParameter("clientRequests", this.clientRequests)
+		    .addParameter("stylistId", this.stylistId)
+		    .executeUpdate()
+		    .getKey();
+		}
+	}
+
+	public static Client find(int id) {
+	    try(Connection con = DB.sql2o.open()){
+		    String sql = "SELECT * FROM clients WHERE id=:id";
+		    Client client = con.createQuery(sql)
+		    .addParameter("id", id)
+		    .executeAndFetchFirst(Client.class);
+		    return client;
+    	}
+  	}
+}
+
+
+
+
+
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,1 +1,72 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class ClientTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteReviewsQuery = "DELETE FROM clients *;";
+      con.createQuery(deleteReviewsQuery).executeUpdate();
+    }
+  }
+
+  @Test
+  public void client_instantiatesCorrectly_true(){
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1);
+    assertEquals(true, client instanceof Client);
+  }
+
+  @Test
+  public void clientGetName_getName_String(){
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1);
+    assertEquals("Justin", client.getName());
+  }
+
+  @Test
+  public void clientPhone_getPhone_String(){
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1);
+    assertEquals("253-448-1364", client.getPhone());
+  }
+
+  @Test
+  public void clientGetRequestedDate_getRequestedDate_String(){
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1);
+    assertEquals("06-16-2016", client.getRequestedDate());
+  }
+
+  @Test
+  public void clientGetRequests_getClientRequests_String(){
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1);
+    assertEquals("Client needs a little of the top, no biggie", client.getClientRequests());
+  }
+
+  @Test
+  public void save_clientSavedToStylist_int(){
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1); 
+    client.saveClientToStylist(5);
+    assertEquals(Client.all().get(0).getId(), client.getId());
+  }
+
+  @Test
+  public void find_findReviewById_int() {
+    Client client = new Client("Justin", "253-448-1364", "06-16-2016", "Client needs a little of the top, no biggie", 1); 
+    client.saveClientToStylist(5);
+    Client clientId = Client.find(client.getId());
+    assertEquals("Justin", clientId.getName());
+  }
+}
+
+
+
+
 
